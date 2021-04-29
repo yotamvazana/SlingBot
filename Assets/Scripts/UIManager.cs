c9d32d87dfffad76efc2148dff7e915c89dce90e
Roza13-bit <roizamir876@gmail.com>
2021-04-29 10:55:47 +0300

    SlingBot 29/04/2021 - MainMenu UI Updated.
    SlingBot 29/04/2021 - Main Menu UI Updated.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,7 +6,91 @@ namespace UIManager
 {
     public class UIManager : MonoBehaviour
     {
-        
+        private const string MainMenuPanel = "MainMenuReset";
+
+        private const string collectionPanel = "CollectionPanel";
+        [SerializeField] private GameObject collectionPanelGO;
+
+        private const string settingsPanel = "SettingsPanel";
+        [SerializeField] private GameObject settingsPanelGO;
+
+        private const string storePanel = "StorePanel";
+        [SerializeField] private GameObject storePanelGO;
+
+        private const string afterAdPanel = "AfterAdPanel";
+        [SerializeField] private GameObject afterAdPanelGO;
+
+        [SerializeField] private GameObject resetPanelGO;
+
+        private void PanelsUpdateMainMenu(string panelName)
+        {
+            switch (panelName)
+            {
+                case collectionPanel:
+
+                    resetPanelGO.SetActive(true);
+                    collectionPanelGO.SetActive(true);
+                    settingsPanelGO.SetActive(false);
+                    storePanelGO.SetActive(false);
+                    afterAdPanelGO.SetActive(false);
+
+                    break;
+
+                case settingsPanel:
+
+                    resetPanelGO.SetActive(true);
+                    collectionPanelGO.SetActive(false);
+                    settingsPanelGO.SetActive(true);
+                    storePanelGO.SetActive(false);
+                    afterAdPanelGO.SetActive(false);
+
+                    break;
+
+                case storePanel:
+
+                    resetPanelGO.SetActive(true);
+                    collectionPanelGO.SetActive(false);
+                    settingsPanelGO.SetActive(false);
+                    storePanelGO.SetActive(true);
+                    afterAdPanelGO.SetActive(false);
+
+                    break;
+
+                case afterAdPanel:
+
+                    resetPanelGO.SetActive(true);
+                    collectionPanelGO.SetActive(false);
+                    settingsPanelGO.SetActive(false);
+                    storePanelGO.SetActive(false);
+                    afterAdPanelGO.SetActive(true);
+
+                    break;
+
+                case MainMenuPanel:
+
+                    resetPanelGO.SetActive(false);
+                    collectionPanelGO.SetActive(false);
+                    settingsPanelGO.SetActive(false);
+                    storePanelGO.SetActive(false);
+                    afterAdPanelGO.SetActive(false);
+
+                    break;
+
+            }
+
+        }
+
+        public void OnClickCollectionButton()
+        { PanelsUpdateMainMenu(collectionPanel); }
+
+        public void OnClickSettingsButton()
+        { PanelsUpdateMainMenu(settingsPanel); }
+
+        public void OnClickStoreButton()
+        { PanelsUpdateMainMenu(storePanel); }
+
+        public void OnClickResetPanel()
+        { PanelsUpdateMainMenu(MainMenuPanel); }
 
     }
 
