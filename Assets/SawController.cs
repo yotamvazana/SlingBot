b2ab0e86e34a794b9b06a5b41c2aa138c5e1b246
Roza13-bit <roizamir876@gmail.com>
2021-05-27 16:42:29 +0300

    *Added Saw, Flip Doors, Spinning Doors.
    *Added Saw, Flip Doors, Spinning Doors.

---
+++ Assets/SawController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawController : MonoBehaviour
+{
+    public float sawSpeed = 1;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(-Vector3.forward * sawSpeed);
+
+    }
+
+}
